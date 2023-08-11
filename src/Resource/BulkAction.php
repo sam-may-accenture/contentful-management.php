17cdef37d7de1af452c0176e92bb93ec0d26b91b
May, Samuel <samuel.may@accenture.com>
2023-08-11 16:22:24 -0400

    create base bulk action resource

---
+++ src/Resource/BulkAction.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Contentful\Management\Resource;
+
+use Contentful\Management\SystemProperties\BulkAction as SystemProperties;
+use Contentful\Core\Api\Link;
+use Contentful\Management\Resource\Behavior\CreatableInterface;
+use function GuzzleHttp\json_encode as guzzle_json_encode;
+
+class BulkAction extends BaseResource 
+{
+    /**
+     * @var SystemProperties
+     */
+    protected $sys;
+
+    /**
+     * @var string
+     */
+    protected $action;
+
+    /**
+     * @var Link[]
+     */
+    protected $items;
+
+    /**
+     * @var array
+     */
+    protected $error;
+
+    public function getSystemProperties(): SystemProperties
+    {
+        return $this->sys;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function jsonSerialize(): mixed
+    {
+      return [
+        'sys' => $this->sys,
+        'action' => $this->action,
+        'payload' => [
+          'entities' => [
+            'items' => $this->items,
+          ]
+        ],
+        'error' => $this->error
+      ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function asUriParameters(): array
+    {
+        return [
+            'space' => $this->sys->getSpace()->getId(),
+            'environment' => $this->sys->getEnvironment()->getId(),
+            'action' => $this->sys->getId(),
+        ];
+    }
+}
