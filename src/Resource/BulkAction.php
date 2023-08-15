b63875f254436772d6b7501150d0648db79c184d
May, Samuel <samuel.may@accenture.com>
2023-08-15 17:09:07 -0400

    getters for object properties

--- src/Resource/BulkAction.php
+++ src/Resource/BulkAction.php
@@ -7,62 +7,80 @@ use Contentful\Core\Api\Link;
 use Contentful\Management\Resource\Behavior\CreatableInterface;
 use function GuzzleHttp\json_encode as guzzle_json_encode;
 
-class BulkAction extends BaseResource 
+class BulkAction extends BaseResource
 {
-    /**
-     * @var SystemProperties
-     */
-    protected $sys;
+  /**
+   * @var SystemProperties
+   */
+  protected $sys;
 
-    /**
-     * @var string
-     */
-    protected $action;
+  /**
+   * @var string
+   */
+  protected $action;
 
-    /**
-     * @var Link[]
-     */
-    protected $items;
+  /**
+   * @var Link[]
+   */
+  protected $items;
 
-    /**
-     * @var array
-     */
-    protected $error;
+  /**
+   * @var array
+   */
+  protected $error;
 
-    public function getSystemProperties(): SystemProperties
-    {
-        return $this->sys;
-    }
+  public function getSystemProperties(): SystemProperties
+  {
+    return $this->sys;
+  }
 
-    /**
-     * {@inheritdoc}
-     */
-    public function jsonSerialize(): mixed
-    {
-      return [
-        'sys' => $this->sys,
-        'action' => $this->action,
-        'payload' => [
-          'entities' => [
-            'sys' => [
-              'type' => 'Array'
-            ],
-            'items' => $this->items,
-          ]
-        ],
-        'error' => $this->error
-      ];
-    }
+  /**
+   * {@inheritdoc}
+   */
+  public function jsonSerialize(): mixed
+  {
+    return [
+      'sys' => $this->sys,
+      'action' => $this->action,
+      'payload' => [
+        'entities' => [
+          'sys' => [
+            'type' => 'Array'
+          ],
+          'items' => $this->items,
+        ]
+      ],
+      'error' => $this->error
+    ];
+  }
 
-    /**
-     * {@inheritdoc}
-     */
-    public function asUriParameters(): array
-    {
-        return [
-            'space' => $this->sys->getSpace()->getId(),
-            'environment' => $this->sys->getEnvironment()->getId(),
-            'action' => $this->sys->getId(),
-        ];
-    }
+  /**
+   * {@inheritdoc}
+   */
+  public function asUriParameters(): array
+  {
+    return [
+      'space' => $this->sys->getSpace()->getId(),
+      'environment' => $this->sys->getEnvironment()->getId(),
+      'action' => $this->sys->getId(),
+    ];
+  }
+
+  /**
+   * @return Link[]
+   */
+  public function getItems(): array
+  {
+    return $this->items ?? [];
+  }
+
+  public function getError(): array
+  {
+    return $this->error ?? [];
+  }
+
+  public function getAction(): string
+  {
+    return $this->action;
+  }
 }
